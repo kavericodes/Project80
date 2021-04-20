31e8ac73d5f08465d8f6fb6e4148408b06784b2c
Kaveri <77133605+kavericodes@users.noreply.github.com>
2021-04-19 19:29:37 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 
 function getParagraph1() {
-    var inputs = [];
+    var input = [];
     for (var i = 1; i <= 6; i++) {
-        inputs.push(document.getElementById("para1_input_box_" + i).value);
+        input.push(document.getElementById("para1_input_box_" + i).value);
     }
-    inputs.join(". ");
-    document.getElementById("showParagraph1").innerHTML = inputs.join(". ");
+    document.getElementById("showParagraph1").innerHTML = input.join(". ");
 }
 
 function getParagraph2 () {
@@ -13,7 +12,12 @@ function getParagraph2 () {
     for (var j = 1; j <= 6; j++) {
         input.push(document.getElementById("para2_input_box_" + j).value);
     }
-    input.join(". ");
     document.getElementById("showParagraph2").innerHTML = input.join(". ");
 }
+
+
+
+
+
+//function join_arrays () { var join_arrays1 = [];}//
 
